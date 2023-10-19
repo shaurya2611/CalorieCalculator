18d27c4f152de6726cd90d89de2b28f20cd80457
Shaurya Singh <shaurya2611@gmail.com>
2023-10-19 17:59:41 +0530

    Initial Commit

---
+++ CalorieCalculator/CalorieCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieCalculatorApp.swift
+//  CalorieCalculator
+//
+//  Created by Shaurya Singh on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CalorieCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
